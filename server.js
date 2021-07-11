cd91a38afd023a69a3e3fde012c1fe6d249ec008
Ivcota <ivcotad@gmail.com>
2021-07-11 10:02:41 -0700

    Added Connection to MongoDB Database

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
-
-// Route Files
-const bootcamps = require("./routes/bootcamps");
+const connectDB = require("./config/db");
+const colors = require("colors");
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
+
+// Connect to Database
+connectDB();
+
+// Route Files
+const bootcamps = require("./routes/bootcamps");
 
 const app = express();
 
@@ -19,9 +24,18 @@ app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(
-    `Server is running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`
+    `Server is running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`.yellow.bold
   )
 );
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close Server and exit process
+  server.close(() => {
+    process.exit(1);
+  });
+});
